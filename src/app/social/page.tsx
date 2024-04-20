c227a14ede2c53f1d9184c5d8c8acbc31efc4b96
Riley-26 <rileyread1@gmail.com>
2024-04-20 01:13:41 +0100

    grid styling - main

--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -4,41 +4,46 @@ export default function Page(){
     return (
         <div className="flex w-full h-full">
             <div className="flex flex-col h-full w-1/3 m-1">
-                <div className="h-1/3 w-full p-4 flex flex-col m-1 rounded-lg bg-[rgb(35,35,35)]">
-                    <div className="w-full h-full bg-[rgb(45,45,45)] rounded-lg p-4">
+                <div className="h-1/3 w-full card">
+                    <div className="innerCard">
                         <h1>Friend count</h1>
                     </div>
                 </div>
-                <div className="h-2/3 w-full p-4 flex flex-col m-1 rounded-lg bg-[rgb(35,35,35)]">
-                    <h1 className="text-2xl">FRIEND LIST</h1>
-                    <div className="w-full h-full bg-[rgb(45,45,45)] rounded-lg p-4">
+                <div className="h-2/3 w-full card">
+                    <h1 className="h1Bg">FRIEND LIST</h1>
+                    <hr className="hrShadow" />
+                    <div className="innerCard">
                     </div>
                 </div>
             </div>
             <div className="flex flex-col h-full w-1/3 m-1">
-                <div className="h-2/3 w-full p-4 flex flex-col m-1 rounded-lg bg-[rgb(35,35,35)]">
-                    <h1 className="text-2xl">MY SHARED NOTES</h1>
-                    <div className="w-full h-full bg-[rgb(45,45,45)] rounded-lg mt-2 p-4">
+                <div className="h-2/3 w-full card">
+                    <h1 className="h1Bg">MY SHARED NOTES</h1>
+                    <hr className="hrShadow" />
+                    <div className="innerCard mt-2">
 
                     </div>
                 </div>
-                <div className="h-1/3 w-full p-4 flex flex-col m-1 rounded-lg bg-[rgb(35,35,35)]">
-                    <h1 className="text-2xl">FRIEND REQUESTS</h1>
-                    <div className="w-full h-full bg-[rgb(45,45,45)] rounded-lg mt-2 p-4">
+                <div className="h-1/3 w-full card">
+                    <h1 className="h1Bg">FRIEND REQUESTS</h1>
+                    <hr className="hrShadow" />
+                    <div className="innerCard mt-2">
 
                     </div>
                 </div>
             </div>
             <div className="flex flex-col h-full w-1/3 m-1">
-                <div className="h-2/3 w-full p-4 flex flex-col m-1 rounded-lg bg-[rgb(35,35,35)]">
-                    <h1 className="text-2xl">JOINED NOTES</h1>
-                    <div className="w-full h-full bg-[rgb(45,45,45)] rounded-lg mt-2 p-4">
+                <div className="h-2/3 w-full card">
+                    <h1 className="h1Bg">JOINED NOTES</h1>
+                    <hr className="hrShadow" />
+                    <div className="innerCard mt-2">
 
                     </div>
                 </div>
-                <div className="h-1/3 w-full p-4 flex flex-col m-1 rounded-lg bg-[rgb(35,35,35)]">
-                    <h1 className="text-2xl">RECENTLY ADDED</h1>
-                    <div className="w-full h-full bg-[rgb(45,45,45)] rounded-lg mt-2 p-4">
+                <div className="h-1/3 w-full card">
+                    <h1 className="h1Bg">RECENTLY ADDED</h1>
+                    <hr className="hrShadow" />
+                    <div className="innerCard mt-2">
 
                     </div>
                 </div>
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -4,30 +4,33 @@ export default function Page(){
     return (
         <div className="flex flex-col w-full h-full">
             <div className="flex w-full h-2/3">
-                <div className="w-1/3 p-4 flex flex-col m-1 rounded-lg bg-[rgb(35,35,35)]">
-                    <h1 className="text-2xl">WELCOME, RILEY</h1>
-                    <div className="w-full h-full bg-[rgb(45,45,45)] rounded-lg mt-2 p-4">
+                <div className="w-1/3 card">
+                    <h1 className="h1Bg">WELCOME, RILEY</h1>
+                    <hr className="hrShadow" />
+                    <div className="innerCard mt-2 p-4">
                         <h2 className="text-center">Lighting mode</h2>
 
                     </div>
                 </div>
-                <div className="w-2/3 p-4 flex flex-col m-1 rounded-lg bg-[rgb(35,35,35)]">
-                    <h1 className="text-2xl">STATS</h1>
-                    <div className="w-full h-full bg-[rgb(45,45,45)] rounded-lg mt-2 p-4">
+                <div className="w-2/3 card">
+                    <h1 className="h1Bg">STATS</h1>
+                    <hr className="hrShadow" />
+                    <div className="innerCard mt-2">
                         
                     </div>
                 </div>
             </div>
             <div className="flex w-full h-1/3">
-                <div className="w-2/3 p-4 flex flex-col m-1 rounded-lg bg-[rgb(35,35,35)]">
-                    <h1 className="text-2xl">MY RECENT</h1>
-                    <div className="w-full h-full bg-[rgb(45,45,45)] rounded-lg mt-2 p-4">
+                <div className="w-2/3 card">
+                    <h1 className="h1Bg">MY RECENT</h1>
+                    <hr className="hrShadow" />
+                    <div className="innerCard mt-2">
 
                     </div>
                 </div>
-                <div className="w-1/3 p-4 flex flex-col m-1 rounded-lg bg-[rgb(35,35,35)] items-center justify-center text-center">
-                    <h1 className="text-2xl hover:text-blue-300 transition-all my-4 cursor-pointer py-2 px-4 bg-[rgb(45,45,45)] rounded-lg max-w-[350px]">CHANGE USERNAME/PASSWORD</h1>
-                    <h1 className="text-2xl hover:text-red-400 transition-all my-4 cursor-pointer py-2 px-4 bg-[rgb(45,45,45)] rounded-lg">LOG OUT</h1>
+                <div className="w-1/3 card items-center justify-center text-center">
+                    <h1 className="text-2xl hover:text-blue-300 transition-all my-4 cursor-pointer py-2 px-4 rounded-lg max-w-[350px]">CHANGE USERNAME/PASSWORD</h1>
+                    <h1 className="text-2xl hover:text-red-400 transition-all my-4 cursor-pointer py-2 px-4 rounded-lg">LOG OUT</h1>
                 </div>
             </div>
         </div>
