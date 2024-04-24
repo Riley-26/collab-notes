b017c791b3798fb5d3dfee25dcb3fbc1c0faf797
Riley-26 <rileyread1@gmail.com>
2024-04-25 00:31:28 +0100

    Bugs - main

--- src/app/ui/Navbar.tsx
+++ src/app/ui/Navbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import clsx from "clsx";
 
 export default function Navbar({loggedIn}: {loggedIn:boolean}){
 
@@ -24,11 +25,11 @@ export default function Navbar({loggedIn}: {loggedIn:boolean}){
                     />
                 </div>
                 <ul id="navTabs" className="flex justify-between xl:min-w-[35%] min-w-[50%] text-lg">
-                    <li><Link className="hover:text-blue-300 transition-all" href="/about">ABOUT</Link></li>
+                    <li><Link className={clsx("hover:text-blue-300 transition-all", {"text-blue-300": currentTab === "/about"})} href="/about">ABOUT</Link></li>
                     <span className="w-[2px] h-full bg-neutral-700 bg-opacity-60"/>
-                    <li><Link className="hover:text-blue-300 transition-all" href="/notes">NOTES</Link></li>
-                    <li><Link className="hover:text-blue-300 transition-all" href="/social">SOCIAL</Link></li>
-                    <li><Link className="hover:text-blue-300 transition-all" href={ loggedIn ? "/user" : "/login" }>{ loggedIn ? "USER" : "LOG IN" }</Link></li>
+                    <li><Link className={clsx("hover:text-blue-300 transition-all", {"text-blue-300": currentTab === "/notes"})} href="/notes">NOTES</Link></li>
+                    <li><Link className={clsx("hover:text-blue-300 transition-all", {"text-blue-300": currentTab === "/social"})} href="/social">SOCIAL</Link></li>
+                    <li><Link className={clsx("hover:text-blue-300 transition-all", {"text-blue-300": currentTab === ("/user" || "/login")})} href={ loggedIn ? "/user" : "/login" }>{ loggedIn ? "USER" : "LOG IN" }</Link></li>
                 </ul>
             </nav>
             <hr className="hrShadow" />
