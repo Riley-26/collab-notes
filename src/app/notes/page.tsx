cb68c26ff217571da95af4d89fbfe4727b824c6a
Riley-26 <rileyread1@gmail.com>
2024-05-20 00:44:56 +0100

    grid for user page - main

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -6,6 +6,11 @@ export default function Page(){
     return (
         <div className="p-12">
             <h1 className="text-3xl">Notes</h1>
+            <div className="">
+                <div className="">
+                    
+                </div>
+            </div>
         </div>
     )
 }
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -2,8 +2,19 @@
 
 export default function Page(){
     return (
-        <div className="p-12">
+        <div className="p-12 w-full">
             <h1 className="text-3xl">User</h1>
+            <div className="grid grid-flow-row grid-cols-3 h-full py-8">
+                <div className="card h-full">
+                    <h1>card</h1>
+                </div>
+                <div className="card h-full">
+                    <h1>card</h1>
+                </div>
+                <div className="card h-full">
+                    <h1>card</h1>
+                </div>
+            </div>
         </div>
     )
 }
