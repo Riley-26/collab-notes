fb8740175e58e41c1e67b57705e3c7a422f4cdf3
Riley Read <rileyread1@gmail.com>
2024-05-21 17:52:57 +0100

    create note details - mac

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -4,18 +4,15 @@ export default function Page(){
     return (
         <div className="p-12 w-full">
             <h1 className="text-3xl">User</h1>
-            <div className="flex justify-between w-[100%] h-[100%]">
-                <div className="grid grid-flow-col grid-rows-2 py-8 px-2 gap-4 min-w-[65%]">
+            <div className="flex flex-col justify-between w-[100%] h-[100%] py-8">
+                <div className="grid grid-flow-col grid-rows-1 gap-4 h-[49%]">
                     <div className="card h-full">
                         <h2>Stats</h2>
                     </div>
+                </div>
+                <div className="grid grid-flow-row grid-cols-2 gap-4 h-[49%]">
                     <div className="card h-full">
                         <h2>Latest</h2>
-                    </div>
-                </div>
-                <div className="grid grid-flow-col grid-rows-2 py-8 px-2 gap-4 min-w-[35%]">
-                    <div className="card">
-                        <h2>Settings</h2>
                     </div>
                     <div className="card">
                         <h2>Settings</h2>
