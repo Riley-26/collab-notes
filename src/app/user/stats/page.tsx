7a3f7ba5fb63debbf69a1c8e902f319c2fd93f44
Riley-26 <rileyread1@gmail.com>
2024-05-27 01:10:45 +0100

    User stats page ui - main

--- src/app/user/stats/page.tsx
+++ src/app/user/stats/page.tsx
@@ -2,8 +2,26 @@
 
 export default function Page(){
     return (
-        <div className="p-12">
+        <div className="p-12 w-full">
             <h1 className="text-3xl">Stats</h1>
+            <div className="flex flex-col justify-between w-[100%] h-[100%] py-8">
+                <div className="grid grid-flow-row grid-cols-1 px-2 gap-4 min-w-[65%] min-h-[49%]">
+                    <div className="card h-full">
+                        <h2>Stats</h2>
+                    </div>
+                </div>
+                <div className="grid grid-flow-row grid-cols-3 px-2 gap-4 min-w-[35%] min-h-[49%]">
+                    <div className="card h-full">
+                        <h2>Settings</h2>
+                    </div>
+                    <div className="card h-full">
+                        <h2>Settings</h2>
+                    </div>
+                    <div className="card h-full">
+                        <h2>Settings</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
